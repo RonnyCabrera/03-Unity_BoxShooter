f34897bb07aae751e957f03adc456064c6322d23
Ronny Cabrera <ronny-38@hotmail.com>
2018-05-10 15:53:01 -0500

    Creacion BoxShooter

---
+++ BoxShooter/Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour {
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	void Update () {
+        if (doSpin)
+        {
+            //Girar alrededor del eje Y
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+        if (doMotion)
+        {
+            //Moverse arriba y abajo
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }
+    }
+}
